0eb0517c8a4ea75e6fa9cb78b54920b941ed5b8d
Alzpetka <lili3.pokorna@seznam.cz>
2022-04-21 11:48:12 +0200

    validations

--- oarepo_loose_validation_builder/property_preprocessors/validity.py
+++ oarepo_loose_validation_builder/property_preprocessors/validity.py
@@ -12,9 +12,8 @@ class ValidityPreprocessor(PropertyPreprocessor):
     @process(
         model_builder=JSONSchemaBuilder,
         path="**/properties/*",
-        # condition=lambda current, stack: current.type != "xy",
     )
-    def modify_fulltext_schema(self, data, stack: ModelBuilderStack, **kwargs):
+    def modify_json_schema(self, data, stack: ModelBuilderStack, **kwargs):
         try:
             if 'required' in data:
                 data.pop('required')
@@ -46,6 +45,30 @@ class ValidityPreprocessor(PropertyPreprocessor):
         model_builder=InvenioRecordSchemaBuilder,
         path="**/properties/*",
     )
-    def modify_fulltext_marshmallow(self, data, stack: ModelBuilderStack, **kwargs):
+    def modify_validators_marshmallow(self, data, stack: ModelBuilderStack, **kwargs):
 
+        try:
+            loose_validators = []
+            errors = []
+            if 'oarepo:validity' in data and 'non-structural' in data['oarepo:validity']:
+                errors = data['oarepo:validity']['non-structural']
+
+            if 'oarepo:marshmallow' in data and 'required' in data['oarepo:marshmallow'] and data['oarepo:marshmallow']['required']:
+                if 'required' in errors:
+                    data['oarepo:marshmallow']['required'] = 'OarepoError("required", struct=False)'
+                else:
+                    data['oarepo:marshmallow']['required'] = 'OarepoError("required", struct=True)'
+
+            if 'oarepo:marshmallow' in data and 'oarepo:validity' in data and 'validators' in data['oarepo:marshmallow']:
+                for v in data['oarepo:marshmallow']['validators']:
+                    if v.startswith('ma_valid.Length') and 'length' in errors:
+                        v = v.replace('ma_valid.', 'loose_valid.')
+                    elif v.startswith('ma_valid.Range') and 'range' in errors:
+                        v = v.replace('ma_valid.', 'loose_valid.')
+                    elif v.startswith('ma_valid.OneOf') and 'enum' in errors:
+                        v = v.replace('ma_valid.', 'loose_valid.')
+                    loose_validators.append(v)
+                data['oarepo:marshmallow']['validators'] = loose_validators
+        except:
+            pass
         return data
